ad0bd138c1d7a7942d88659062229ee72648b706
Daniel Slater <danielslater811@hotmail.com>
2015-10-08 11:36:17 +1100

    Committing the other files for the same thing as before

--- dvjudge/browse.py
+++ dvjudge/browse.py
@@ -1,6 +1,7 @@
 from flask import render_template, session, request, abort
 from dvjudge import app
 from core import query_db
+from comments import get_comments, post_comment
 
 @app.route('/browse', methods=['GET'])
 def browse():
@@ -20,7 +21,7 @@ def browse_search():
     # Pass only those on
     return render_template('browse.html', challenges=results, searchterm=request.form.get('searchterm'))
 
-@app.route('/browse/<problem_name>', methods=['GET'])
+@app.route('/browse/<problem_name>', methods=['GET', 'POST'])
 def browse_specific_problem(problem_name): 
     cur = query_db('select * from challenges where name = ?', [problem_name], one=True)
     if cur is not None:
@@ -33,7 +34,13 @@ def browse_specific_problem(problem_name):
     else:
         abort(404)
     problem_info = {'problem_id': problem_id, 'name': name, 'description': description, 'sample_tests': sample_tests, 'input_desc': input_desc, 'output_desc': output_desc}
-    
+
+    #check for posted comment
+    if request.method == 'POST':
+        comment = request.form['comment']
+        if comment:
+            post_comment(session['user'], problem_id, comment)
+
     #Check if it's a redirect from submission and the program 
     #has produced output
     #Stored in session cookie
@@ -49,4 +56,7 @@ def browse_specific_problem(problem_name):
     else:
         code = None
 
-    return render_template('problem.html', problem_info=problem_info, output=info, code = code )
+    #insert the comments section
+    question_comments = get_comments(problem_id)
+
+    return render_template('problem.html', problem_info=problem_info, output=info, code = code, problem_name=problem_name, comments=question_comments)
