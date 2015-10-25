efe28d6a36752931e3f0686e55719876d952e528
TaraTjandra <tara_tjandra@hotmail.com>
2015-10-25 18:46:31 +1100

    Minor site clean ups. Fixing small errors.

--- dvjudge/community_browse.py
+++ dvjudge/community_browse.py
@@ -5,30 +5,21 @@ from browse import do_browse, do_browse_post
 
 @app.route('/community/browse', methods=['GET'])
 def community_browse():
-    # cur = query_db('select id, name from challenges where com_flag = 1')
-    # challenges = [dict(id=row[0],name=row[1]) for row in cur]
-    # # Retrieve category names
-    # cur = query_db('select name from categories');
-    # if cur:
-    #     categories = [dict(name=row[0]) for row in cur]
 
     vals = do_browse(com=True)
     return render_template('browse.html', challenges=vals[0], categories=vals[1], com_flag=True)
-    # return redirect(url_for('browse', com=True))
-    # return render_template('browse.html', challenges=challenges, categories=categories, com_flag=True)
 
 @app.route('/community/browse', methods=['POST'])
 def community_browse_post():
     
-    cur = query_db('select id, name from challenges where com_flag = 1')
+    cur = query_db('select id, name, submitter_id from challenges where com_flag = 1')
     # Produce an array of hashes that looks something like:
     # [{id->'1', name->'some challenge name'}, {other hash}]  
-    challenges = [dict(id=row[0],name=row[1]) for row in cur]
+    challenges = [dict(id=row[0],name=row[1],submitter_id=row[2]) for row in cur]
     # Retrieve category names
     cur = query_db('select name from categories');
     if cur:
         categories = [dict(name=row[0]) for row in cur]
-
 
     if request.form.get('add') is not None:
         # Admin request to move challenges
@@ -38,16 +29,24 @@ def community_browse_post():
                 if move_name:
                     move = "update challenges set com_flag=0 where name=?;"
                     update_db(move, [challenge['name']])
-            cur = query_db('select id, name from challenges where com_flag = 1')
-            challenges = [dict(id=row[0],name=row[1]) for row in cur]
+            cur = query_db('select id, name, submitter_id from challenges where com_flag = 1')
+            challenges = [dict(id=row[0],name=row[1],submitter_id=row[2]) for row in cur]
     # Admin request to delete challenge
     elif request.form.get('delete_chal') is not None:
         if session['user'] == "admin":
             update_db('delete from challenges where name=?',[request.form.get('delete_chal')])
-            cur = query_db('select id, name from challenges where com_flag = 1')
+            cur = query_db('select id, name, submitter_id from challenges where com_flag = 1')
             # Produce an array of hashes that looks something like:
             # [{id->'1', name->'some challenge name'}, {other hash}]  
-            challenges = [dict(id=row[0],name=row[1]) for row in cur]
+            challenges = [dict(id=row[0],name=row[1],submitter_id=row[2]) for row in cur]
+
+    # Convert user_ids to usernames
+    for challenge in challenges:
+        lookup = query_db("select username from users where id = ?", [challenge['submitter_id']], one=True)
+        if lookup is not None:
+            challenge['submitter_id'] = lookup[0]
+        else:
+            challenge['submitter_id'] = "DvJudge"
 
     vals = do_browse_post(com=True)
     return render_template('browse.html', challenges=vals[0], searchterm=vals[1], categories=vals[2], no_completed=vals[3], com_flag=True)
@@ -56,31 +55,3 @@ def community_browse_post():
 def community_browse_specific_challenge(challenge_name):
 
     return redirect(url_for('browse_specific_challenge', challenge_name=challenge_name))
-    # cur = query_db('select * from challenges where name = ?', [challenge_name], one=True)
-    # if cur is not None:
-    #     challenge_id  = cur[0]
-    #     name        = cur[1]
-    #     description = cur[2]
-    #     sample_tests= cur[5]
-    #     input_desc  = cur[6]
-    #     output_desc = cur[7]
-    # else:
-    #     abort(404)
-    # challenge_info = {'challenge_id': challenge_id, 'name': name, 'description': description, 'sample_tests': sample_tests, 'input_desc': input_desc, 'output_desc': output_desc}
-    
-    # #Check if it's a redirect from submission and the program 
-    # #has produced output
-    # #Stored in session cookie
-    # if 'output' in session:
-    #     info = session['output']
-    #     session.pop('output', None)
-    # else:
-    #     info = None
-    # #check for submitted code from user
-    # if 'code' in session:
-    #     code = session['code']
-    #     session.pop('code', None)
-    # else:
-    #     code = None
-
-    # return render_template('challenge.html', challenge_info=challenge_info, output=info, code = code )
