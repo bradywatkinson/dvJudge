3d27b15b3bfcbfe64cbc0ab809424054c44be94f
Brady Watkinson <brady.a.watkinson@unsw.edu.au>
2015-10-25 18:52:46 +1100

    Fixed bug with profile being only current one

--- dvjudge/db_populate.sql
+++ dvjudge/db_populate.sql
@@ -41,20 +41,20 @@ insert into challenges (name,description,input,output,sample_tests,input_desc,ou
 
 -- Accounts
 -- dannyeei:daniel
-insert into users (username, email, password, salt, solved_challenges)
-	values ('dannyeei', 'danielslater811@hotmail.com', 'd9bdde89547d18f0d6bd72d9a17133ae2cb023357357f51c5d226cbf2d02a056e89102bed4c29e0f2827a952027c9e0a3c2b3545735f0a1c643427d81fc75281', 'thisissamplesalt', '1');
+insert into users (username, email, password, salt, solved_challenges, image)
+	values ('dannyeei', 'danielslater811@hotmail.com', 'd9bdde89547d18f0d6bd72d9a17133ae2cb023357357f51c5d226cbf2d02a056e89102bed4c29e0f2827a952027c9e0a3c2b3545735f0a1c643427d81fc75281', 'thisissamplesalt', '1', '1_profilepic.jpg');
 
 -- typical:typical
 insert into users (username, email, password, salt)
 	values ('typical', 'typical@hotmail.com', 'dcaa9d20adff97448541892dd0f13536e3379e2503b03b67411a69d902237d02b17dc74a9f955f831532d449104004f8151b52f09ddf8c2b18e1d78dc720ade1', 'saltandpepper');
 
 -- admin:default
-insert into users (username, email, password, salt)
-	values ('admin', 'admin@hotmail.com', '0b491227c0328e4c87c03fe4749b0b3a78d6da7094f5ef249b99caa759c6f1b1b6c952abb73e9448c5468fb7635f2d90a2f83b14e466c95b19b100fb9aec3c19', 'yumyumsaltinmytum');
+insert into users (username, email, password, salt, image)
+	values ('admin', 'admin@hotmail.com', '0b491227c0328e4c87c03fe4749b0b3a78d6da7094f5ef249b99caa759c6f1b1b6c952abb73e9448c5468fb7635f2d90a2f83b14e466c95b19b100fb9aec3c19', 'yumyumsaltinmytum', '3_profilepic.png');
 
 -- stanley:default
-insert into users (username, email, password, salt, solved_challenges)
-	values ('stanley', 'stanleyhon348@gmail.com', '0b491227c0328e4c87c03fe4749b0b3a78d6da7094f5ef249b99caa759c6f1b1b6c952abb73e9448c5468fb7635f2d90a2f83b14e466c95b19b100fb9aec3c19', 'yumyumsaltinmytum', '1|6');
+insert into users (username, email, password, salt, solved_challenges, image)
+	values ('stanley', 'stanleyhon348@gmail.com', '0b491227c0328e4c87c03fe4749b0b3a78d6da7094f5ef249b99caa759c6f1b1b6c952abb73e9448c5468fb7635f2d90a2f83b14e466c95b19b100fb9aec3c19', 'yumyumsaltinmytum', '1|6', '4_profilepic.jpg');
 
 -- Submissions
 insert into submissions (user_id, challenge_id, status, status_info, language, code)
--- dvjudge/profile.py
+++ dvjudge/profile.py
@@ -20,18 +20,18 @@ def public_profile(user):
         user_id     = cur[0]
         username    = cur[1]
         email       = cur[2]
+        imagename   = cur[6]
+        print imagename
         if cur[5] is not None:
             solved_challenges = cur[5]
             query_string = "id = " + solved_challenges.replace("|", " or id = ")
-            print query_string
             cur = query_db('select id, name from challenges where %s' % query_string)
             challenges = [dict(id=row[0],name=row[1],completed=True) for row in cur]
-            print challenges
-            return render_template('public_profile.html',userid=user_id,username=username,email=email,challenges=challenges)
+            return render_template('public_profile.html',username=username,challenges=challenges,imagename=imagename)
     else:
         return render_template('user_dne.html',username=user)
 
-    return render_template('public_profile.html',userid=user_id,username=user,email=email)
+    return render_template('public_profile.html',username=user,imagename=imagename)
 
 @app.route('/profile', methods=['GET'])
 def profile():
